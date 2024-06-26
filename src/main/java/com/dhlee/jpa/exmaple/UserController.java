fed6770ef5a8894d9cb25754ef9dfa449c429a6d
dhlee <dhlee@eactive.co.kr>
2024-06-26 16:16:31 +0900

    add actuator, security

--- src/main/java/com/dhlee/jpa/exmaple/UserController.java
+++ src/main/java/com/dhlee/jpa/exmaple/UserController.java
@@ -1,5 +1,8 @@
 package com.dhlee.jpa.exmaple;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,10 +11,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-
-import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/users")
